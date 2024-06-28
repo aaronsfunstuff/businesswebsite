c0c048830c2584ab7ee78d5923b9ace4e803ad53
aaronsfunstuff <135081747+aaronsfunstuff@users.noreply.github.com>
2024-06-28 15:55:16 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($) {
+
+   $('.links-animation').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
